fca0820fef8531c7ade542fef7ddd4f7796e58f8
DSokolov <DSokolov@sportmasterlab.net>
2023-11-12 21:18:52 +0300

    create component Note

---
+++ src/components/Note.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="note">
+		<p class="note__title">
+			{{ note.title }}
+		</p>
+		<ul class="tasks__wrapper">
+			<Task v-for="task in note.tasks" :task="task" />
+		</ul>
+	</div>
+</template>
+
+<script setup lang="ts">
+import Task from '@/components/Task.vue';
+
+defineProps<{ note: object }>();
+
+const tasks = note.tasks
+</script>
+
+<style lang="stylus">
+.note
+  border 1px solid #D9D9D9
+  border-radius 8px
+</style>
