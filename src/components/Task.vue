a8a70c015eecf2fdfcceed50686b33c0a6cd14b2
DSokolov <DSokolov@sportmasterlab.net>
2023-11-12 21:19:10 +0300

    create component Task

---
+++ src/components/Task.vue
@@ -0,0 +1,16 @@
+<template>
+	<li class="task">
+		<input type="checkbox" name="task" :checked="task.isDone" />
+		<label for="task">{{ task.label }}</label>
+	</li>
+</template>
+
+<script setup lang="ts">
+defineProps<{ task: object }>();
+</script>
+
+<style lang="stylus">
+.task
+  text-align start
+  list-style none
+</style>
